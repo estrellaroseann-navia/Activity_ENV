4e6280c2ade843983ef2d9bbb32e365b8f54aba2
estrellaroseann_navia <202180026@psu.palawan.edu.ph>
2024-04-05 11:04:30 +0800

    modify allowed host

--- PSUSphere/projectsite/projectsite/settings.py
+++ PSUSphere/projectsite/projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-eibmr3nsbow*b6*_vez)zg&hx-n%f5z&txjr2cl!nf5^k@fdq(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
