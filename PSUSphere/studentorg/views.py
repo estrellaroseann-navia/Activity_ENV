f50a2c06348f08761d30568980b42577c7997563
estrellaroseann_navia <202180026@psu.palawan.edu.ph>
2024-04-16 11:33:32 +0800

    Add Organization feature

--- PSUSphere/studentorg/views.py
+++ PSUSphere/studentorg/views.py
@@ -1,10 +1,15 @@
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from studentorg.models import Organization
+from studentorg.forms import OrganizationForm
+from django.urls import reverse_lazy
 
 template_name = "home.hmtl"
 
-class OrganizationList(ListView):
+paginated_by=5
+
+class OrganizationCreateView(CreateView):
     model = Organization
-    context_object_name = 'organization'
-    template_name = "org_list.html"
-    paginate_by = 5
+    form_class = OrganizationForm
+    template_name = "org_add.html"
+    success_url = reverse_lazy('organization-list')
