219ddb2fddc3ee39a794eb17ac3e0014cfbd0233
estrellaroseann_navia <202180026@psu.palawan.edu.ph>
2024-04-16 22:40:26 +0800

    Edit Organization

--- PSUSphere/studentorg/views.py
+++ PSUSphere/studentorg/views.py
@@ -1,5 +1,5 @@
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from studentorg.models import Organization
 from studentorg.forms import OrganizationForm
 from django.urls import reverse_lazy
@@ -8,8 +8,19 @@ template_name = "home.hmtl"
 
 paginated_by=5
 
+class HomePageView(ListView):
+        model = Organization
+        context_object_name = 'home'
+        template_name = "home.html"
+
 class OrganizationCreateView(CreateView):
     model = Organization
     form_class = OrganizationForm
     template_name = "org_add.html"
+    success_url = reverse_lazy('organization-list')
+
+class OrganizationCreateView(UpdateView):
+    model = Organization
+    form_class = OrganizationForm
+    template_name = "org_edit.html"
     success_url = reverse_lazy('organization-list')
