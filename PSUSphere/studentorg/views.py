05e0023aa2eaaa30ff077010e05e512df06351ad
estrellaroseann_navia <202180026@psu.palawan.edu.ph>
2024-04-16 10:40:00 +0800

    Organization added

--- PSUSphere/studentorg/views.py
+++ PSUSphere/studentorg/views.py
@@ -1,7 +1,10 @@
 from django.views.generic.list import ListView
 from studentorg.models import Organization
 
-class HomePageView(ListView):
+template_name = "home.hmtl"
+
+class OrganizationList(ListView):
     model = Organization
-    context_object_name = 'home'
-    template_name = "home.html"
+    context_object_name = 'organization'
+    template_name = "org_list.html"
+    paginate_by = 5
