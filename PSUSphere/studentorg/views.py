2117155100a76fe545d718156ba43f8c83cc4faa
estrellaroseann_navia <202180026@psu.palawan.edu.ph>
2024-04-18 21:28:50 +0800

    code tracing huhu

--- PSUSphere/studentorg/views.py
+++ PSUSphere/studentorg/views.py
@@ -1,21 +1,27 @@
+from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from studentorg.models import Organization
 from studentorg.forms import OrganizationForm
 from django.urls import reverse_lazy
 
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
-
 from typing import Any
 from django.db.models.query import QuerySet
 from django.db.models import Q
 
-template_name = "home.hmtl"
+class OrganizationList(ListView):
+    template_name = 'org_list.html'
+    paginate_by = 5
 
-@method_decorator(login_required, name='dispatch')
+    def get_queryset(self, *args, **kwargs):
+        qs = super(OrganizationList, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get("q") != None:
+            query = self.request.GET.get('q')
+            qs = qs.filter(Q(name__icontains=query) |
+                               Q(description__icontains=query))
+            return qs
 
-class HomePageViewt(ListView):
+class HomePageView(ListView):
         model = Organization
         context_object_name = 'home'
         template_name = "home.html"
@@ -24,16 +30,7 @@ class OrganizationList(ListView):
         model = Organization
         context_object_name = 'organization'
         template_name = 'org_list.html'
-        paginated_by=5
-
-
-        def get_queryset(self, *args, **kwargs):
-            qs = super(OrganizationList, self).get_queryset(*args, **kwargs)
-            if self.request.GET.get("q") != None:
-                query = self.request.GET.get('q')
-                qs = qs.filter(Q(name__icontains=query) |
-                               Q(description__icontains=query))
-                return qs
+        paginated_by = 5
 
 class OrganizationCreateView(CreateView):
     model = Organization
@@ -41,11 +38,30 @@ class OrganizationCreateView(CreateView):
     template_name = "org_add.html"
     success_url = reverse_lazy('organization-list')
 
-class OrganizationCreateView(UpdateView):
+
+class OrganizationUpdateView(UpdateView):
     model = Organization
     form_class = OrganizationForm
     template_name = "org_edit.html"
     success_url = reverse_lazy('organization-list')
+
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
+
+
+template_name = "home.hmtl"
+
+@method_decorator(login_required, name='dispatch')
+
+
+
+
+
+
+
+
+
 
 class OrganizationUpdateView(UpdateView):
     model = Organization
